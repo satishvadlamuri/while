f4268f4d2b758723077f24bbf96d19dfbf23bfbf
root <root@ip-172-31-39-199.us-east-2.compute.internal>
2019-07-11 10:11:25 +0000

    while

---
+++ src/main/java/com/maven/www/App.java
@@ -0,0 +1,18 @@
+package com.maven.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+public class WhileExample {  
+public static void main(String[] args) {  
+    int i=1;  
+    while(i<=10){  
+        System.out.println(i);  
+    i++;  
+    }  
+}  
+}  
+}
